4e98b8b47b4382a5768df97adae24be07ba17ae7
Vinicius Farias <fsvinicius9@gmail.com>
2025-01-30 17:18:56 -0300

    Stripe payment

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -46,6 +46,18 @@
                                     </form>
                                 </div>
 
+
+                                <div class="col-md-4 my-2">
+                                    <form action="{{route('stripe.payment')}}" method="POST">
+                                        @csrf
+                                        <input type="hidden" value="40" name="price">
+                                        <button type="submit" class="btn btn-outline-dark flex-shrink-0" type="button">
+                                            <i class="bi-cart-fill me-1"></i>
+                                            Buy With Stripe
+                                        </button>
+                                    </form>
+                                </div>
+
                             </div>
 
                     </div>
